0f02e2a07949dce2470c3e5ca44d057decdba12b
Iwansal64 <iwansal64@gmail.com>
2026-05-16 12:44:51 +0700

    [hotifx] Sidebar Links Content Update

--- src/components/SidebarNavigation.svelte
+++ src/components/SidebarNavigation.svelte
@@ -1,8 +1,9 @@
 <ul class="active-after-opening fixed delay-2250 duration-1250 -left-4 top-1/2 -translate-x-full [&.active]:translate-x-10 -translate-y-1/2 z-100 h-max flex flex-col gap-10">
-      <li><a class="font-['Saira',calibri] tracking-wider duration-250 text-white/50 hover:text-white" href="#fact">The Fact</a></li>
-      <li><a class="font-['Saira',calibri] tracking-wider duration-250 text-white/50 hover:text-white" href="#mirror">The Mirror</a></li>
-      <li><a class="font-['Saira',calibri] tracking-wider duration-250 text-white/50 hover:text-white" href="#numbers">The Numbers</a></li>
-      <li><a class="font-['Saira',calibri] tracking-wider duration-250 text-white/50 hover:text-white" href="#pledge">The Pledge</a></li>
-      <li><a class="font-['Saira',calibri] tracking-wider duration-250 text-white/50 hover:text-white" href="#mirror">The Quiz</a></li>
-      <li><a class="font-['Saira',calibri] tracking-wider duration-250 text-white/50 hover:text-white" href="#mirror">The Shift</a></li>
+      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#fact">The Fact</a></li>
+      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#mirror">The Mirror</a></li>
+      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#numbers">The Numbers</a></li>
+      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#pledge">The Pledge</a></li>
+      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#quiz">The Quiz</a></li>
+      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#shift">The Shift</a></li>
+      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#pledge">The Pledge</a></li>
 </ul>
