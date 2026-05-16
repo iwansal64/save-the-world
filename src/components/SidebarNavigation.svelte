155f2dcb7e773330025bcec19fd09296e2bf58b9
Iwansal64 <iwansal64@gmail.com>
2026-05-16 12:21:08 +0700

    [flag] Add Sidebar

---
+++ src/components/SidebarNavigation.svelte
@@ -0,0 +1,8 @@
+<ul class="active-after-opening fixed delay-2250 duration-1250 -left-4 top-1/2 -translate-x-full [&.active]:translate-x-10 -translate-y-1/2 z-100 h-max flex flex-col gap-10">
+      <li><a class="font-['Saira',calibri] tracking-wider duration-250 text-white/50 hover:text-white" href="#fact">The Fact</a></li>
+      <li><a class="font-['Saira',calibri] tracking-wider duration-250 text-white/50 hover:text-white" href="#mirror">The Mirror</a></li>
+      <li><a class="font-['Saira',calibri] tracking-wider duration-250 text-white/50 hover:text-white" href="#numbers">The Numbers</a></li>
+      <li><a class="font-['Saira',calibri] tracking-wider duration-250 text-white/50 hover:text-white" href="#pledge">The Pledge</a></li>
+      <li><a class="font-['Saira',calibri] tracking-wider duration-250 text-white/50 hover:text-white" href="#mirror">The Quiz</a></li>
+      <li><a class="font-['Saira',calibri] tracking-wider duration-250 text-white/50 hover:text-white" href="#mirror">The Shift</a></li>
+</ul>
