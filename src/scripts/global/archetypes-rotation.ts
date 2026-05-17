8be34c170e22704fa71c3c8cf9788c67448d374e
Iwansal64 <iwansal64@gmail.com>
2026-05-17 20:13:47 +0700

    [flag] Add mirror animation in TheMirror section

--- src/scripts/global/archetypes-rotation.ts
+++
@@ -1,9 +0,0 @@
-/* ═══════════════════════════════ ARCHETYPES ROTATION ═══════════════════════════════ */
-const ats = document.querySelectorAll(".archetype-text");
-let atI = 0;
-ats[0].classList.add("active");
-setInterval(() => {
-      ats[atI].classList.remove("active");
-      atI = (atI + 1) % ats.length;
-      ats[atI].classList.add("active");
-}, 2000);
