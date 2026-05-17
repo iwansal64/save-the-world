ba8db75abbb0d1a4352d7616c7e86dd993bc99a3
Iwansal64 <iwansal64@gmail.com>
2026-05-17 12:17:15 +0700

    [tiny] Add border for sidebar navigation hover effect

--- src/components/SidebarNavigation.svelte
+++ src/components/SidebarNavigation.svelte
@@ -1,23 +1,23 @@
 <ul id="sidebar-navigation" class="active-after-opening fixed delay-2250 duration-1250 -left-4 top-1/2 -translate-x-full [&.active]:translate-x-10 -translate-y-1/2 z-100 h-max flex flex-col gap-10">
-      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
-            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#fact">The Fact</a>
+      <li class="duration-250 px-4 py-1 border border-transparent hover:border-(--accent-color) [&.active]:*:text-(--amber)! [&.active]:*:opacity-100! [&.active]:hover:border-(--amber)">
+            <a class="text-(--accent-color) font-bold text-sm font-['Saira',calibri] tracking-wider duration-250" href="#fact">The Fact</a>
       </li>
-      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
-            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#mirror">The Mirror</a>
+      <li class="duration-250 px-4 py-1 border border-transparent hover:border-(--accent-color) [&.active]:*:text-(--amber)! [&.active]:*:opacity-100! [&.active]:hover:border-(--amber)">
+            <a class="text-(--accent-color) font-bold text-sm font-['Saira',calibri] tracking-wider duration-250" href="#mirror">The Mirror</a>
       </li>
-      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
-            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#numbers">The Numbers</a>
+      <li class="duration-250 px-4 py-1 border border-transparent hover:border-(--accent-color) [&.active]:*:text-(--amber)! [&.active]:*:opacity-100! [&.active]:hover:border-(--amber)">
+            <a class="text-(--accent-color) font-bold text-sm font-['Saira',calibri] tracking-wider duration-250" href="#numbers">The Numbers</a>
       </li>
-      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
-            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#quiz">The Quiz</a>
+      <li class="duration-250 px-4 py-1 border border-transparent hover:border-(--accent-color) [&.active]:*:text-(--amber)! [&.active]:*:opacity-100! [&.active]:hover:border-(--amber)">
+            <a class="text-(--accent-color) font-bold text-sm font-['Saira',calibri] tracking-wider duration-250" href="#quiz">The Quiz</a>
       </li>
-      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
-            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#lifecycle">The LifeCycle</a>
+      <li class="duration-250 px-4 py-1 border border-transparent hover:border-(--accent-color) [&.active]:*:text-(--amber)! [&.active]:*:opacity-100! [&.active]:hover:border-(--amber)">
+            <a class="text-(--accent-color) font-bold text-sm font-['Saira',calibri] tracking-wider duration-250" href="#lifecycle">The LifeCycle</a>
       </li>
-      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
-            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#shift">The Shift</a>
+      <li class="duration-250 px-4 py-1 border border-transparent hover:border-(--accent-color) [&.active]:*:text-(--amber)! [&.active]:*:opacity-100! [&.active]:hover:border-(--amber)">
+            <a class="text-(--accent-color) font-bold text-sm font-['Saira',calibri] tracking-wider duration-250" href="#shift">The Shift</a>
       </li>
-      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
-            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#pledge">The Pledge</a>
+      <li class="duration-250 px-4 py-1 border border-transparent hover:border-(--accent-color) [&.active]:*:text-(--amber)! [&.active]:*:opacity-100! [&.active]:hover:border-(--amber)">
+            <a class="text-(--accent-color) font-bold text-sm font-['Saira',calibri] tracking-wider duration-250" href="#pledge">The Pledge</a>
       </li>
 </ul>
