d88b3168a11855cd90449ad6e3965240d900f700
Iwansal64 <iwansal64@gmail.com>
2026-05-17 10:23:05 +0700

    [flag] Sidebar navigation effect whenever the user reach some point

--- src/components/SidebarNavigation.svelte
+++ src/components/SidebarNavigation.svelte
@@ -1,9 +1,23 @@
-<ul class="active-after-opening fixed delay-2250 duration-1250 -left-4 top-1/2 -translate-x-full [&.active]:translate-x-10 -translate-y-1/2 z-100 h-max flex flex-col gap-10">
-      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#fact">The Fact</a></li>
-      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#mirror">The Mirror</a></li>
-      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#numbers">The Numbers</a></li>
-      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#pledge">The Pledge</a></li>
-      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#quiz">The Quiz</a></li>
-      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#shift">The Shift</a></li>
-      <li><a class="font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#pledge">The Pledge</a></li>
+<ul id="sidebar-navigation" class="active-after-opening fixed delay-2250 duration-1250 -left-4 top-1/2 -translate-x-full [&.active]:translate-x-10 -translate-y-1/2 z-100 h-max flex flex-col gap-10">
+      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
+            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#fact">The Fact</a>
+      </li>
+      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
+            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#mirror">The Mirror</a>
+      </li>
+      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
+            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#numbers">The Numbers</a>
+      </li>
+      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
+            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#quiz">The Quiz</a>
+      </li>
+      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
+            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#lifecycle">The LifeCycle</a>
+      </li>
+      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
+            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#shift">The Shift</a>
+      </li>
+      <li class="[&.active]:*:text-(--amber)! [&.active]:*:opacity-100!">
+            <a class="text-sm font-['Saira',calibri] tracking-wider duration-250 opacity-50 hover:opacity-100" href="#pledge">The Pledge</a>
+      </li>
 </ul>
